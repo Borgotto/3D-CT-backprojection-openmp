5d126bd93c9925df62cf8ad45e60a5b680d04481
Emanuele Borghini <manuborg2002@gmail.com>
2024-08-17 20:11:45 +0200

    Create profiler shell script

---
+++ profiling/runProfiler.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# get the directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# go back one directory to the root of the project
+cd "$DIR"/.. || exit
+
+# compile program
+make
+
+# run program
+./backprojector "$@"
+
+# snapshot name
+SNAPSHOT_NAME="profiling/snapshots/$(ls -l ./profiling/snapshots/ | wc -l) - $(date '+%Y-%m-%d %H:%M:%S')"
+
+# run profiler
+gprof backprojector | gprof2dot -n0 -e0 | dot -Tsvg -Gbgcolor=transparent -o "$SNAPSHOT_NAME".svg
+
+# convert svg to png
+#convert -background white -alpha remove -alpha off "$SNAPSHOT_NAME".svg "$SNAPSHOT_NAME".png
