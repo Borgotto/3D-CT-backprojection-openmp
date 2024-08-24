18aa9f90fe11d15bc5575e5e2edeeb9fb8984e95
Emanuele Borghini <manuborg2002@gmail.com>
2024-08-24 21:19:19 +0200

    Update documentation

--- docs/structvolume.js
+++ docs/structvolume.js
@@ -1,16 +1,16 @@
 var structvolume =
 [
     [ "coefficients", "structvolume.html#a8553556d086a75fc0e97218bafa95279", null ],
-    [ "nPlanesX", "structvolume.html#ad13e1a281aa26081f77308a586fd76ef", null ],
     [ "nPlanesX", "structvolume.html#aac2f5e7622f62d43ed8e77597192d0a9", null ],
-    [ "nPlanesY", "structvolume.html#aad811081005b27f02cc36053057fe83a", null ],
     [ "nPlanesY", "structvolume.html#ae7c2d51655c43cc67c415ff6a10d356d", null ],
-    [ "nPlanesZ", "structvolume.html#a048293cabea1587bbe5ceac07f97954d", null ],
     [ "nPlanesZ", "structvolume.html#a6f19dc2bd44138b9a5b0837d194c68a7", null ],
     [ "nVoxelsX", "structvolume.html#acc234723ff240e5a1b51bb03dd68b4d4", null ],
     [ "nVoxelsX", "structvolume.html#ae173f58c13ce3ec7624e87a87209ccc1", null ],
     [ "nVoxelsY", "structvolume.html#ad95c2211e460d55788f6abe42846e2b0", null ],
     [ "nVoxelsY", "structvolume.html#a4e7a6d8240dd62a2e1ffb5c1f5d36874", null ],
     [ "nVoxelsZ", "structvolume.html#adfd57b1bda7a29adf33896f70334654e", null ],
-    [ "nVoxelsZ", "structvolume.html#af9c926521a6b62779faf2d992a479e47", null ]
+    [ "nVoxelsZ", "structvolume.html#af9c926521a6b62779faf2d992a479e47", null ],
+    [ "voxelSizeX", "structvolume.html#a90005f328de8fb0618df028986b012a9", null ],
+    [ "voxelSizeY", "structvolume.html#abfb83b19efd7fdc1202fb6361d2d09ba", null ],
+    [ "voxelSizeZ", "structvolume.html#a03c7c75ed91fa81edf642866bd9c791f", null ]
 ];
