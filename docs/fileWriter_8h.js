7aee55fd0d9c03b90fc8ffeefc6f46689592e461
Emanuele Borghini <manuborg2002@gmail.com>
2024-09-09 18:00:51 +0200

    Update documentation

--- docs/fileWriter_8h.js
+++ docs/fileWriter_8h.js
@@ -2,6 +2,6 @@ var fileWriter_8h =
 [
     [ "volume", "structvolume.html", "structvolume" ],
     [ "volume", "fileWriter_8h.html#a8d58bee42bf93a193595267ce3e933c0", null ],
-    [ "writeVolumeNRRD", "fileWriter_8h.html#a25c018aea36b43f77e3f14af4ba7a6f6", null ],
-    [ "writeVolumeRAW", "fileWriter_8h.html#a0fc77da284b460f4ab96af666194aa42", null ]
+    [ "writeVolumeNRRD", "fileWriter_8h.html#a6b67947c4cdb01abad830fd1cf4392c4", null ],
+    [ "writeVolumeRAW", "fileWriter_8h.html#ab47e9ce2de4f8ba260db443f8fe7c30d", null ]
 ];
